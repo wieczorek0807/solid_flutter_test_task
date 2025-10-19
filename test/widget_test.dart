e541a6415c8d5a685fe335098834517d73e002c3
wieczorek0807 <wieczorek0807@gmail.com>
2025-10-19 14:27:57 +0200

    refactor pubspec.yaml and update widget test

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -11,20 +11,16 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:solid_flutter_test_task/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets(
+    'Random Color Background app smoke test',
+    (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(const RandomColorApp());
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    // Verify that "Hello there" text is displayed.
+    expect(find.text('Hello there'), findsOneWidget);
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    // Verify that the screen is tappable (GestureDetector is present).
+    expect(find.byType(GestureDetector), findsOneWidget);
   });
 }
